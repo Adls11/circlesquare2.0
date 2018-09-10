d754128d75cbf293892efa5d29fd40728de42e6f
AdlanSaidullaev <adlan.saidullaev11@gmail.com>
2018-09-10 18:13:25 +0300

    Анимация меню

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+//Обязатеьно оптимизиторвать js скрипт для слайдера
+// Сделать общую функцию для добавлению слайдеров без
+// правки скрипта
+
+/* Слайдер для блока кейсов */
+
+
+
+//скроллинг
+
+/*
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  alert(scrolled);
+}
+
+*/
+
+
+
+
+
